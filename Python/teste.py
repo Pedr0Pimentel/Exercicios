4f27581c724aac01c8806a5655715d71d4ccb9d7
Pedro Pimentel Torres <pedrohenriqueptorres10@gmail.com>
2025-10-31 15:39:28 -0300

    Update teste.py

--- Python/teste.py
+++ Python/teste.py
@@ -1,6 +1,4 @@
-notas = [8.5,4.0,10.0,6.5,7.0]
-notas_aprovadas=[]
-for nota in notas:
-    if nota>=7.0:
-        notas_aprovadas.append(nota)
-print(notas_aprovadas)
+def vezes_2(numero):
+    return numero*2
+
+print(vezes_2(4))
