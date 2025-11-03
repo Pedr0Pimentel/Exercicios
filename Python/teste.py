02f24dc94625fb622488835a7fe875141c6898a9
Pedro Pimentel Torres <pedrohenriqueptorres10@gmail.com>
2025-11-03 15:58:18 -0300

    Update teste.py

---
+++ Python/teste.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+while num<=9:
+    print('Número inválido, por favor tente novamente!')
+    num = int(input('Digite um número: '))
+print('Finalmente! Um número válido!')
